81c8411a108ebc03777157bf732f3e7fc1d5001c
Mauro Faria <m.r.d.faria@gmail.com>
2024-09-28 20:29:10 +0200

    Checkpoint

--- beamer/sources/game.cpp
+++ beamer/sources/game.cpp
@@ -1,7 +1,9 @@
 #include "raylib.h"
 #include "swept-aabb.h"
 #include "operators.h"
-#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdexcept>
 
 class Game {
 public:
@@ -22,23 +24,118 @@ public:
     {
         while (isRunning) 
         {
-            Update();  // Handle game logic
+            float deltaTime = GetFrameTime();
+            Update(deltaTime);  // Handle game logic
             Draw();    // Render game visuals
         }
     }
 
+    wchar_t GetTile(int x, int y)
+    {
+        if (x>=0 && x < nLevelWidth && y >= 0 && y < nLevelHeight)
+            return sLevel[y * nLevelWidth + x];
+        else 
+            return L' ';
+    }
+
+    void SetTile(int x, int y, wchar_t c)
+    {
+        if (x>=0 && x < nLevelWidth && y >= 0 && y < nLevelHeight)
+            sLevel[y * nLevelWidth + x] = c;
+    }
+
+    void InitTileMap()
+    {
+        bool widthDetected = false;
+        for (size_t i = 1; i < sLevel.size(); i++)
+        {
+            if (sLevel[i] == L'\n')
+            {
+                nLevelHeight++;
+                widthDetected = true;
+            }
+            else if(!widthDetected)
+            {
+                nLevelWidth++;
+            }
+        }
+        for (auto i = sLevel.begin(); i != sLevel.end();)
+        {
+            if (*i == L'\n')  
+            {
+                i = sLevel.erase(i);  
+            }
+            else
+            {
+                ++i;
+            }
+        }
+        if (sLevel.size() != nLevelHeight * nLevelWidth) 
+        {
+            throw std::runtime_error("Error: Tilemap size does not match the expected dimensions!");
+        }
+    }
 
 private:
+    std::wstring sLevel = 
+LR"(
+.....#.......
+.....#.......
+.....#.......
+.....#.......
+.....#.......
+.....#.......
+)";
+
+    int nLevelWidth = 0;
+    int nLevelHeight = 0;
+    int nTileWidth = 64;
+    int nTileHeight = 64;
+    float fCameraPosX = 0.0f;
+    float fCameraPosY = 0.0f;
+    int nVisibleTilesX = 0;
+    int nVisibleTilesY = 0;
+    Vector2 playerPos = Vector2{0.0f, 0.0f};
+    Camera2D camera;
+
     void Init() 
     {
+
+        InitTileMap();
         InitWindow(screenWidth, screenHeight, title);
         SetTargetFPS(60);
         isRunning = true;
+
+
+        camera.rotation = 0.0f;
+        camera.zoom = 1.0f;
+
     }
 
+    void Update(float delta) 
+    { 
+	    nVisibleTilesX = screenWidth / nTileWidth;
+		nVisibleTilesY = screenHeight / nTileHeight;
+        camera.target = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+ 
+        camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+        playerPos.x = screenWidth / 2;
+        playerPos.y = screenWidth /2 ;
 
-    void Update() 
-    {
+        if (IsKeyDown(KEY_RIGHT)) playerPos.x += nTileWidth;
+        else if (IsKeyDown(KEY_LEFT)) playerPos.x -= nTileWidth;
+
+        // Camera reset (zoom and rotation)
+        if (IsKeyPressed(KEY_R))
+        {
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
+        }
+            // Camera zoom controls
+        camera.zoom += ((float)GetMouseWheelMove()*0.05f);
+
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
         if (WindowShouldClose()) 
         {
             isRunning = false;
@@ -49,29 +146,45 @@ private:
     void Draw() 
     {
         BeginDrawing();
-        ClearBackground(BLUE);
+        ClearBackground(WHITE);
+        BeginMode2D(camera);
+        int total = 0;
+        int tilesToDraw;
 
-        Vector2 vMouse = {float(GetMouseX()), float(GetMouseY())};
-        Rectangle r = {100.0f,100.0f, 100.0f, 200.0f};
-        Vector2 ray_point = {20.0f, 20.0f};
+        // Calculate the bounds of the camera view in world space
+        // note, camera.target is the camera in the world coordinates
+        float camLeft = camera.target.x - (screenWidth * 0.5f / camera.zoom);
+        float camRight = camera.target.x + (screenWidth * 0.5f / camera.zoom);
+        float camTop = camera.target.y - (screenHeight * 0.5f / camera.zoom);
+        float camBottom = camera.target.y + (screenHeight * 0.5f / camera.zoom);
 
-        Vector2 ray_direction = vMouse - ray_point;
-        DrawLineV(ray_point, vMouse, GREEN);
 
-        Vector2 cp, cn;
-        float t;
-
-        if (helper::RayVsRect(ray_point, ray_direction, r, cp, cn, t) && t < 1)
+        // Loop through the tiles and only draw those within the camera view
+        for (int x = 0; x < nLevelWidth; x++)
         {
-            DrawRectangleRec(r, YELLOW);
-            DrawCircleV(cp, 10.0, RED);
-            DrawLineV(cp, cp+(cn*Vector2{50, 50}), YELLOW);
+            for (int y = 0; y < nLevelHeight; y++)
+            {
+                float tileX = x * nTileWidth;
+                float tileY = y * nTileHeight;
 
-        }
-        else
-            DrawRectangleRec(r, WHITE);  
-        printf("t hit near is %f\n", t);
-
+                // Check if the tile is within the camera bounds before drawing (draw two extra tile layers outside of camera)
+                if (tileX + 2*nTileWidth >= camLeft && tileX <= camRight &&
+                    tileY + 2*nTileHeight >= camTop && tileY <= camBottom)
+                {
+                    auto sTileId = GetTile(x, y);
+                    if (sTileId == L'#')
+                    {
+                        DrawRectangle(tileX, tileY, nTileWidth, nTileHeight, BROWN);
+                    }
+                    else 
+                    {
+                        DrawRectangleLines(tileX, tileY, nTileWidth, nTileHeight, BLACK);
+                    }
+                }
+            }
+        }    
+        EndMode2D();
+        DrawRectangle(0, 0, nTileWidth, nTileHeight, YELLOW);  
         EndDrawing();
     }
 
@@ -84,7 +197,7 @@ private:
 
 
 int main() {
-    Game myGame(800, 450, "My Raylib Game");
+    Game myGame(800, 600, "My Raylib Game");
 
     myGame.Run();
 
